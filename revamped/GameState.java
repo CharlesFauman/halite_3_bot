3110f8db2c9a5d20744830201bf7c97746db7632
charles fauman <charlesfauman@gmail.com>
2019-01-12 12:23:41 -0500

    gucci

--- revamped/GameState.java
+++ revamped/GameState.java
@@ -1,24 +1,85 @@
 package revamped;
 
+import java.util.HashMap;
+
+import hlt.EntityId;
 import hlt.Game;
 import hlt.Log;
+import hlt.Position;
+import hlt.Ship;
 
 public class GameState {
+	
+	enum State {
+		 RETURNING(0), GATHERING(1);
+
+		private final int value;
+
+		private State(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+	}
 
 	Game game;
+	HashMap<EntityId, State> ship_states;
+	
+	
 	public GameState() {
 		game = new Game();
-		game.ready("KnewBot");
+		
+		// initialize variables
+		
+		ship_states = new HashMap<EntityId, State>();
+		
+		//
+		
+		
+		
+		
 		Log.log("Made KnewBot");
+		game.ready("KnewBot");
+	}
+	
+	
+	public double getValue(Ship ship, Position position) {
 		
-		// 1. determine value of halite on this current map
+		// distance to position
+		// amount halite in ship
+		// distance to dropoff
+		// value of position
 		
+		return 0;
 	}
 	
 	public void update() {
 		game.updateFrame();
 		
 		int me_halite = game.me.halite;
+		
+		
+		for(Ship ship : game.me.ships.values()) {
+			State current_state = State.GATHERING;
+			if(ship_states.containsKey(ship.id)) {
+				current_state = ship_states.get(ship.id);
+			}
+			
+			switch(current_state) {
+			case GATHERING:
+				
+				break;
+			case RETURNING:
+				
+				break;
+			default:
+				System.err.println("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+				break;
+			}
+			
+		}
 		
 		// decide what ship is going to do
 		// 1. Go for either nearest halite or migrate to location with more halite
