37975e5d07d817a317bddc4c8ff6de8bc4a2c616
a-zuckut <zuckutalex@yahoo.com>
2019-01-12 11:55:48 -0500

    x

--- MyBot.java
+++ MyBot.java
@@ -1,8 +1,7 @@
 
 // This Java API uses camelCase instead of the snake_case as documented in the API docs.
 //     Otherwise the names of methods are consistent.
-import hlt.Game;
-import revamped.*;
+import revamped.GameState;
 
 public class MyBot {
 	public static void main(final String[] args) {
--- revamped/GameState.java
+++ revamped/GameState.java
@@ -11,14 +11,25 @@ public class GameState {
 		game.ready("KnewBot");
 		Log.log("Made KnewBot");
 		
+		// 1. determine value of halite on this current map
+		
 	}
 	
 	public void update() {
 		game.updateFrame();
 		
+		int me_halite = game.me.halite;
+		
 		// decide what ship is going to do
+		// 1. Go for either nearest halite or migrate to location with more halite
+		// 2. If making a dropoff makes sense
+		// 3. If already in a state of doing something
 		
 		// decide if spawn ship
+		// 3 reason why NOT to spawn a ship
+		// 1. Not enough halite on map
+		// 2. We have enough ships
+		// 3. Not enough turns left
 		
 	}
 	
